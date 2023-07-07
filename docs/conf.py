3ffdd50d514488a79d6ef1eaee10c17d42413a08
Evgeni Pandurski <epandurski@gmail.com>
2023-07-07 16:46:22 +0300

    Attempt to fix TOC's navigation_depth

--- docs/conf.py
+++ docs/conf.py
@@ -83,9 +83,7 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {
-    'navigation_depth': 1,
-}
+# html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
