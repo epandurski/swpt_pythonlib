bdff5c4e2a127aed1ba06c0fb17c2bcfde1f077a
Evgeni Pandurski <epandurski@gmail.com>
2024-08-02 13:49:15 +0300

    Improved scanning performance

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Evgeni Pandurski'
 author = 'Evgeni Pandurski'
 
 # The short X.Y version
-version = '0.10.2'
+version = '0.10.3'
 # The full version, including alpha/beta/rc tags
-release = '0.10.2'
+release = '0.10.3'
 
 
 # -- General configuration ---------------------------------------------------
